ed61d6ecf535384cdae4c1f20b4c7c4e181fb64d
24prynk.p <24prynk.p@gmail.com>
2026-01-02 12:57:10 +0530

    APP.js Modified : Message is changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('welcome to shopEasy!');
+    res.send('welcome to shopEasy from priyanka!');
 });
 
 app.listen(PORT, () => {
